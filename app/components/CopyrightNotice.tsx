1abd0e2e36fa5ed3188a31423532b7e7c54ec154
merlinfuchs <merlinfuchs2001@gmail.com>
2026-08-06 10:57:46 +0200

    cleanup

--- app/components/CopyrightNotice.tsx
+++
@@ -1,7 +0,0 @@
-export default function CopyrightNotice() {
-  return (
-    <div className="text-center text-zinc-400 font-light text-sm mt-20">
-      Copyright {new Date().getFullYear()} © Merlin Fuchs
-    </div>
-  );
-}
